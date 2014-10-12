a11ddc595cbd28e8a5524b33bc8ddecab8d6602b
Michal Pasternak <michal.dtz@gmail.com>
2014-10-12 13:43:03 +0200

    This is OLD

--- test_project/manage.py
+++ test_project/manage.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import sys
+
 try:
     from django.core.management import execute_manager
     def boot(settings):
@@ -13,7 +15,6 @@ import imp
 try:
     imp.find_module('settings') # Assumed to be in the same directory.
 except ImportError:
-    import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
     sys.exit(1)
 
